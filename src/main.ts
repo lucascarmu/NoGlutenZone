4b30a5461c8082a07dac29754fc8169963956d13
LucasCarmusciano <lucascarmusciano@gmail.com>
2024-07-09 11:55:50 -0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true
 })
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+  .catch(err => console.error(err));
